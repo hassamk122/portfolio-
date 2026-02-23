79074f6a3ad807c0fc523dcf5870c47424324bc6
hassamk122 <hassamkiani66@gmail.com>
2026-02-23 08:27:38 +0500

    updated projects

--- script/index.js
+++ script/index.js
@@ -30,6 +30,12 @@ fetch("./data/projects.json")
       const tagsContainer = document.createElement("div");
       tagsContainer.className = "project-tags";
 
+      const githubLink = document.createElement("a");
+      githubLink.className = "project-github-link"
+      githubLink.innerHTML = `<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" width=25 height=25 fill="currentColor" ><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <title></title> <g id="Complete"> <g id="arrow-up-right"> <g> <polyline data-name="Right" fill="none" id="Right-2" points="18.7 12.4 18.7 5.3 11.6 5.3" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></polyline> <line fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" x1="5.3" x2="17.1" y1="18.7" y2="6.9"></line> </g> </g> </g> </g></svg>`;
+      githubLink.href = project.link;
+      githubLink.target = "_blank";
+
       project.tags.forEach(tag => {
         const span = document.createElement("span");
         span.className = "project-tag";
@@ -40,6 +46,7 @@ fetch("./data/projects.json")
       card.appendChild(title);
       card.appendChild(description);
       card.appendChild(tagsContainer);
+      card.appendChild(githubLink)
 
       grid.appendChild(card);
     });
